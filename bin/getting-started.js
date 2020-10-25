7f594f8a1e50106ee7bce8f2a1792d70ddc4a64b
unknown <tanthanhkid@gmail.com>
2020-10-25 20:03:16 +0700

    first commit

--- bin/getting-started.js
+++ bin/getting-started.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const cdk = require('@aws-cdk/core');
-const { GettingStartedStack } = require('../lib/getting started-stack');
+const { GettingStartedStack } = require('../lib/getting-started-stack');
 
 const app = new cdk.App();
 new GettingStartedStack(app, 'GettingStartedStack');
